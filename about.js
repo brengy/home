3027bb32a2503a2e8b5d4546fd7d4ef59da82e29
brengy <59324191+brengy@users.noreply.github.com>
2024-06-23 19:03:38 +0300

    Add files via upload

---
+++ about.js
@@ -0,0 +1,20 @@
+
+document.getElementById('mainButton').addEventListener('click', function() {
+    window.location.href = 'main.html';
+});
+
+document.getElementById('monitorButton').addEventListener('click', function() {
+    window.location.href = 'monitor.html';
+});
+
+document.getElementById('gpsButton').addEventListener('click', function() {
+    window.location.href = 'gps.html'; // Update this to the correct HTML file if different
+});
+
+document.getElementById('networkButton').addEventListener('click', function() {
+    window.location.href = 'network.html'; // Update this to the correct HTML file if different
+});
+
+document.getElementById('indButton').addEventListener('click', function() {
+    window.location.href = 'index.html'; // Update this to the correct HTML file if different
+});
